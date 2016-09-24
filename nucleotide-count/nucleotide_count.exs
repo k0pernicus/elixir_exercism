bfe5ada63785568ca01a63a58454b5be3e651a44
Antonin Carette <antonin@MacBook-Air.local>
2016-09-25 00:00:46 +0200

    Add nucleotide-count exercise

---
+++ nucleotide-count/nucleotide_count.exs
@@ -0,0 +1,42 @@
+defmodule NucleotideCount do
+  @nucleotides [?A, ?C, ?G, ?T]
+
+  @doc """
+  Counts individual nucleotides in a NucleotideCount strand.
+
+  ## Examples
+
+  iex> NucleotideCount.count('AATAA', ?A)
+  4
+
+  iex> NucleotideCount.count('AATAA', ?T)
+  1
+  """
+  @spec count([char], char) :: non_neg_integer
+  def count(strand, nucleotide) when nucleotide in @nucleotides do
+    strand?(strand)
+    Enum.count strand, (fn x -> x == nucleotide end)  
+  end
+
+  def count(strand, nucleotide), do: raise(ArgumentError)
+
+
+  @doc """
+  Returns a summary of counts by nucleotide.
+
+  ## Examples
+
+  iex> NucleotideCount.histogram('AATAA')
+  %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
+  """
+  @spec histogram([char]) :: map
+  def histogram(strand) do
+    strand?(strand)
+    Enum.map(@nucleotides, &{&1, count(strand, &1)}) |> Enum.into %{}
+  end
+
+  @spec strand?([char]) :: boolean
+  def strand?(strand) do
+    if not Enum.all?(strand, &(&1 in @nucleotides)), do: raise(ArgumentError)
+  end
+end
